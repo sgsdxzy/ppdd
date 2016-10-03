4990e9d51f284e5c71431e6d81dc8453d08772a0
Light <sgsdxzy@gmail.com>
2016-10-04 00:15:47 +0800

    No need to inherit QWidget class to set layouts

--- ppdd_gui.py
+++ ppdd_gui.py
@@ -24,8 +24,67 @@ class PPDDWindow(QMainWindow):
         self.spectrum_vmax = 1e6        #used to display the amplitude spectrum
         
     def initUI(self): 
-        self.layoutWidget = PPDDLayout()
-        self.setCentralWidget(self.layoutWidget)
+        #using a grid layout Qwidget as centeralwidget
+        self.layout = QWidget()
+        layout = self.layout
+        self.setCentralWidget(layout)
+
+        #divide layout to left and right two columns
+        layout.grid = QGridLayout()
+        layout.setLayout(layout.grid)
+        layout.grid.setColumnStretch(0, 1)
+        layout.grid.setColumnStretch(1, 9)
+        layout.left = QWidget()
+        layout.right = QWidget()
+        layout.grid.addWidget(layout.left, 0, 0)
+        layout.grid.addWidget(layout.right, 0, 1)
+
+        #the left column
+        left = layout.left
+        left.grid = QGridLayout()
+        left.grid.setColumnStretch(0, 3)
+        left.grid.setColumnStretch(1, 7)
+        left.setLayout(left.grid)
+
+        left.grid.addWidget(QLabel(""), 0, 0)
+        left.fx = QLineEdit()
+        left.fx.setValidator(QDoubleValidator(0, 0.5, 6))
+        left.grid.addWidget(left.fx, 0, 1)
+
+        left.grid.addWidget(QLabel("fx"), 0, 0)
+        left.fx = QLineEdit()
+        left.fx.setValidator(QDoubleValidator(0, 0.5, 6))
+        left.grid.addWidget(left.fx, 0, 1)
+
+        left.grid.addWidget(QLabel("fx"), 0, 0)
+        left.fx = QLineEdit()
+        left.fx.setValidator(QDoubleValidator(0, 0.5, 6))
+        left.grid.addWidget(left.fx, 0, 1)
+
+        left.grid.addWidget(QLabel("fy"), 1, 0)
+        left.fy = QLineEdit()
+        left.fy.setValidator(QDoubleValidator(0, 0.5, 6))
+        left.grid.addWidget(left.fy, 1, 1)
+
+        #the right column is used to display 4 plots in (2, 2) style
+        right = layout.right
+        right.grid = QGridLayout()
+        right.grid.setColumnStretch(0, 45)
+        right.grid.setColumnStretch(1, 55)
+        right.setLayout(right.grid)
+
+        right.raw = MplCanvas()
+        right.phase = MplCanvas()
+        right.spectrum = MplCanvas()
+        right.density = MplCanvas()
+
+        right.grid.addWidget(right.raw, 0, 0)
+        right.grid.addWidget(right.phase, 0, 1)
+        right.grid.addWidget(right.spectrum, 1, 0)
+        right.grid.addWidget(right.density, 1, 1)
+
+
+
 
         openFile = QAction(QIcon.fromTheme('document-open'), 'Open', self)
         openFile.setShortcut('Ctrl+O')
@@ -100,21 +159,21 @@ class PPDDWindow(QMainWindow):
                 self.statusBar().showMessage('Failed to find the secondary peak.')
                 return
 
-        self.layoutWidget.conf.fx.setText('{0:.6f}'.format(pyppd.fx))
-        self.layoutWidget.conf.fy.setText('{0:.6f}'.format(pyppd.fy))
+        self.layout.left.fx.setText('{0:.6f}'.format(pyppd.fx))
+        self.layout.left.fy.setText('{0:.6f}'.format(pyppd.fy))
                 
         #plot raw data and selected region
-        ax_raw = self.layoutWidget.display.raw.axes
+        ax_raw = self.layout.right.raw.axes
         ax_raw.set_title('Raw data')
         im1 = ax_raw.pcolormesh(pyppd.rawdata)
         rect1 = patches.Rectangle((pyppd.xmin, pyppd.ymin), pyppd.xmax-pyppd.xmin, pyppd.ymax-pyppd.ymin, linewidth=2, edgecolor='r', facecolor='none')
         ax_raw.set_xlim(0, pyppd.rawdata.shape[1])
         ax_raw.set_ylim(0, pyppd.rawdata.shape[0])
         ax_raw.add_patch(rect1)
-        self.layoutWidget.display.raw.draw()
+        self.layout.right.raw.draw()
 
         #plot amplitude spectrum
-        ax_spectrum = self.layoutWidget.display.spectrum.axes
+        ax_spectrum = self.layout.right.spectrum.axes
         XYf2d_shifted = pyppd.XYf2d_shifted
         xfreq = np.fft.fftshift(np.fft.fftfreq(XYf2d_shifted.shape[1]))
         yfreq = np.fft.fftshift(np.fft.fftfreq(XYf2d_shifted.shape[0]))
@@ -123,7 +182,7 @@ class PPDDWindow(QMainWindow):
         ax_spectrum.set_ylim(-0.2,0.2)
         rect3 = patches.Rectangle((pyppd.fx-pyppd.xband,-np.abs(pyppd.fy)-pyppd.yband), 2*pyppd.xband, 2*(pyppd.yband+np.abs(pyppd.fy)), linewidth=2, edgecolor='r', facecolor='none')
         ax_spectrum.add_patch(rect3)
-        self.layoutWidget.display.spectrum.draw()
+        self.layout.right.spectrum.draw()
 
         pyppd.filt_move()
         #find the center of phase spectrum
@@ -134,14 +193,14 @@ class PPDDWindow(QMainWindow):
             return
 
         #plot phase spectrum
-        ax_phase = self.layoutWidget.display.phase.axes
+        ax_phase = self.layout.right.phase.axes
         ax_phase.set_title('Phase spectrum')
         im2 = ax_phase.pcolormesh(pyppd.phase)
         divider2 = make_axes_locatable(ax_phase)
         cax_phase = divider2.append_axes("right", size="5%", pad=0.05)
         plt.colorbar(im2, cax_phase)
         ax_phase.hlines(pyppd.ycenter, 0, pyppd.phase.shape[1], linewidth=3, colors='black')
-        self.layoutWidget.display.phase.draw()
+        self.layout.right.phase.draw()
 
         #perform abel transform
         try :
@@ -151,7 +210,7 @@ class PPDDWindow(QMainWindow):
             return
 
         #plot desnity (relative refractivity)
-        ax_density = self.layoutWidget.display.density.axes
+        ax_density = self.layout.right.density.axes
         ax_density.set_title('Relative Refractivity')
         im4 = ax_density.pcolormesh(pyppd.AIM, vmax=0.1, vmin=0)
         ax_density.set_xlim(0, pyppd.AIM.shape[1])
@@ -159,7 +218,7 @@ class PPDDWindow(QMainWindow):
         divider4 = make_axes_locatable(ax_density)
         cax_density = divider4.append_axes("right", size="5%", pad=0.05)
         plt.colorbar(im4, cax_density)
-        self.layoutWidget.display.density.draw()
+        self.layout.right.density.draw()
 
     def about(self):
         QMessageBox.about(self, "About", """
@@ -171,78 +230,6 @@ class PPDDWindow(QMainWindow):
 of Education, CAPT, Peking University, Beijing 100871, China
         """)
 
-
-class PPDDLayout(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        self.conf = PPDDConf()
-        self.display = PPDDDisplay()
-
-        self.grid = QGridLayout()
-        self.setLayout(self.grid)
-        self.grid.setColumnStretch(0, 1)
-        self.grid.setColumnStretch(1, 9)
-
-        self.grid.addWidget(self.conf, 0, 0)
-        self.grid.addWidget(self.display, 0, 1)
-
-class PPDDConf(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-        
-    def initUI(self):
-        self.grid = QGridLayout()
-        self.grid.setColumnStretch(0, 3)
-        self.grid.setColumnStretch(1, 7)
-        self.setLayout(self.grid)
-
-        self.grid.addWidget(QLabel(""), 0, 0)
-        self.fx = QLineEdit()
-        self.fx.setValidator(QDoubleValidator(0, 0.5, 6))
-        self.grid.addWidget(self.fx, 0, 1)
-
-        self.grid.addWidget(QLabel("fx"), 0, 0)
-        self.fx = QLineEdit()
-        self.fx.setValidator(QDoubleValidator(0, 0.5, 6))
-        self.grid.addWidget(self.fx, 0, 1)
-
-        self.grid.addWidget(QLabel("fx"), 0, 0)
-        self.fx = QLineEdit()
-        self.fx.setValidator(QDoubleValidator(0, 0.5, 6))
-        self.grid.addWidget(self.fx, 0, 1)
-
-        self.grid.addWidget(QLabel("fy"), 1, 0)
-        self.fy = QLineEdit()
-        self.fy.setValidator(QDoubleValidator(0, 0.5, 6))
-        self.grid.addWidget(self.fy, 1, 1)
-
-
-
-class PPDDDisplay(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-        
-    def initUI(self):
-        grid = QGridLayout()
-        grid.setColumnStretch(0, 45)
-        grid.setColumnStretch(1, 55)
-        self.setLayout(grid)
-
-        self.raw = MplCanvas()
-        self.phase = MplCanvas()
-        self.spectrum = MplCanvas()
-        self.density = MplCanvas()
-
-        grid.addWidget(self.raw, 0, 0)
-        grid.addWidget(self.phase, 0, 1)
-        grid.addWidget(self.spectrum, 1, 0)
-        grid.addWidget(self.density, 1, 1)
-        
 
 class MplCanvas(FigureCanvas):
     """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
