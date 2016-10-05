14d33784882e9c4cbefb076e841ad9054548123c
Light <sgsdxzy@gmail.com>
2016-10-05 19:10:11 +0800

    First working windows package

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
 from distutils.core import setup
 import py2exe
- 
-setup(windows=[{"script":"ppdd_gui.py"}], options={"py2exe":{"includes":["sip"]}})
+import numpy
+import scipy
+
+setup(windows=[{"script":"ppdd_gui.py"}],
+      options={"py2exe":{"includes":['sip',
+                                     'cunwrap._cunwrap',
+                                     'scipy.special._ufuncs_cxx',
+                                     'scipy.linalg.cython_blas',
+                                     'scipy.linalg.cython_lapack',
+                                     'scipy.sparse.csgraph._validation']}})
