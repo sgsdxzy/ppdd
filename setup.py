4673c876964614f612dabf0c95859f3cb8e7cc68
Light <sgsdxzy@gmail.com>
2016-10-06 21:39:59 +0800

    Updated icons

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ from distutils.core import setup
 import py2exe
 import numpy
 import scipy
+import os
 
-setup(windows=[{"script":"ppdd_gui.py"}],
-      options={"py2exe":{"includes":['sip',
+setup(windows=[{"script":"ppdd_gui.py"}, "icon_resources": [(1, "ppdd.ico")]],
+      zipfile=os.path.join(pylibs, library.zip),
+      options={"py2exe":{"optimize": 2,
+                         "includes":['sip',
                                      'cunwrap._cunwrap',
                                      'scipy.special._ufuncs_cxx',
                                      'scipy.linalg.cython_blas',
